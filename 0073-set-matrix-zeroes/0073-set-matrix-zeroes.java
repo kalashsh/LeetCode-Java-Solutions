4dbe187f9e7c9c38f22f3d7ecbcb425c845d2813
Kalash Bansal <kalashb5689@gmail.com>
2026-09-11 19:36:17 +0530

    Time: 1 ms (96.33%), Space: 47.8 MB (24.34%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,45 +1,20 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean firstRowZero = false;
-        boolean firstColZero = false;
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;  
-            }
-        }
+    public void setZeroes(int[][] a) {
+        int m = a.length, n = a[0].length;
+        boolean col0 = false;
         for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
-            }
+            if (a[i][0] == 0) col0 = true;
+
+            for (int j = 1; j < n; j++)
+                if (a[i][j] == 0)
+                    a[i][0] = a[0][j] = 0;
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0; 
-                }
-            }
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if (firstColZero) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--)
+                if (a[i][0] == 0 || a[0][j] == 0)
+                    a[i][j] = 0;
+
+            if (col0) a[i][0] = 0;
         }
     }
 }
