0941ce2be3a0b10d02f829f65991a1f5099e97e4
Kalash Bansal <kalashb5689@gmail.com>
2026-09-20 23:18:43 +0530

    Time: 2 ms (71.46%), Space: 63.5 MB (5.14%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int element=0;
+        for(int num:nums){
+            if(count==0){
+                element=num;
+            }
+            count+=(num==element)?1:-1;
+        }
+        return element;
+    }
+}
